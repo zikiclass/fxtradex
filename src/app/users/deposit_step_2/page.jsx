99378522287023518852677ce8a8212b1052f786
zikiclass <ezekieledafe5@gmail.com>
2024-06-11 00:52:29 -0700

    UI design

--- src/app/users/deposit_step_2/page.jsx
+++ src/app/users/deposit_step_2/page.jsx
@@ -8,8 +8,6 @@ import { DashboardNavbar } from "../../HomeComponents";
 import BottomNavBar from "../_components/BottomNavBar";
 import DashboardPageNavigator from "../../components/DashboardPageNavigator";
 const DepositStep2 = () => {
-  const searchParams = useSearchParams();
-  const amount = searchParams.get("amount");
   const dashboardRef = useRef(null);
   const router = useRouter();
   useEffect(() => {
@@ -23,34 +21,41 @@ const DepositStep2 = () => {
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <div>
-          <DashboardNavbar />
-          <div className="container" style={{ marginTop: "3rem" }}>
-            <DashboardPageNavigator text="Deposit" />
-            <div className="dashboard_" ref={dashboardRef}>
-              <div className="deposit">
-                <h2>
-                  PAY{" "}
-                  {Number(amount).toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "USD",
-                  })}
-                </h2>
-                <p style={{ marginTop: "-20px" }}>SELECT PAYMENT METHOD</p>
-
-                <PanelPlain
-                  title="Send Crypto"
-                  text="send supported crypto currencies"
-                  onClick={handleClick}
-                />
-              </div>
-            </div>
-          </div>
-          <BottomNavBar active="dashboard" />
-        </div>
+        <DepositStep2Content />
       </Suspense>
     </>
   );
 };
 
+const DepositStep2Content = () => {
+  const searchParams = useSearchParams();
+  const amount = searchParams.get("amount");
+  return (
+    <div>
+      <DashboardNavbar />
+      <div className="container" style={{ marginTop: "3rem" }}>
+        <DashboardPageNavigator text="Deposit" />
+        <div className="dashboard_" ref={dashboardRef}>
+          <div className="deposit">
+            <h2>
+              PAY{" "}
+              {Number(amount).toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+              })}
+            </h2>
+            <p style={{ marginTop: "-20px" }}>SELECT PAYMENT METHOD</p>
+
+            <PanelPlain
+              title="Send Crypto"
+              text="send supported crypto currencies"
+              onClick={handleClick}
+            />
+          </div>
+        </div>
+      </div>
+      <BottomNavBar active="dashboard" />
+    </div>
+  );
+};
 export default DepositStep2;
