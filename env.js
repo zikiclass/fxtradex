ab6cf9ef5fb6b6e89c9de14a70d8805eb19c460d
zikiclass <ezekieledafe5@gmail.com>
2025-03-04 04:27:25 -0800

    error handling

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
-export const project_name = "Fx-Tradex";
-export const project_email = "info@fx-tradex.com";
-export const project_link = "fx-tradex.com";
-export const project_http_link = "https://fx-tradex.com";
+export const project_name = "MT5Index-Pro";
+export const project_email = "support@mt5indexpro.com";
+export const project_link = "mt5indexpro.com";
+export const project_http_link = "https://mt5indexpro.com";
 export const project_description =
-  "Forex, Stocks, ETFs and Options | FX-Tradex - Online Trading Platform";
+  "Forex, Stocks, ETFs and Options | MT5Index-Pro - Online Trading Platform";
 export const last_revised = "February 1, 2022";
