ef4c5178db1bea9cabab993c83df0ecb1e76210c
Edafe Ezekiel <45126757+zikiclass@users.noreply.github.com>
2026-01-19 22:57:30 -1100

    Update route.js

--- src/app/api/alladmin/route.js
+++ src/app/api/alladmin/route.js
@@ -1,13 +1,21 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function GET(req) {
   try {
     const admin_records = await prisma.admin.findMany();
     if (admin_records)
       return NextResponse.json(
         { message: "success", admin_records },
-        { status: 200 }
+        {
+    status: 200,
+    headers: {
+      "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+    },
+  }
       );
     return NextResponse.json({ message: "failed" }, { status: 404 });
   } catch (error) {
